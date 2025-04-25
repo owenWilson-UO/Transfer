4625d79fed7f5eb3c573e2a586f5faadf65e6be7
owenWilson-UO <owilson@uoregon.edu>
2025-04-25 00:50:31 -0700

    ability to move with psylink
    https://trello.com/c/KXJyO2pF/16-move-with-psylink

--- Assets/Scripts/PsylinkInteractableObject.cs
+++ Assets/Scripts/PsylinkInteractableObject.cs
@@ -12,18 +12,20 @@ public class PsylinkInteractableObject : MonoBehaviour
     [Header("Settings")]
     [SerializeField] private float speed = 10f;
     public bool isMoving {  get; private set; }
+    private Vector3 velocity = Vector3.zero;
+    private float easeAmount = 2f;
 
     private void Start()
     {
         a = transform.position;
     }
 
-    private void Update()
+    private void FixedUpdate()
     {
 
         //updates the objects position go towards point b if a psylink is attatched, or back to point a if there is no psylink attatched
         Vector3 target = pt.activePsylinks.Any(item => item.obj == gameObject) ? b.position : a;
-        transform.position = Vector3.MoveTowards(transform.position, target, speed * Time.deltaTime);
+        transform.position = Vector3.MoveTowards(transform.position, target, speed * Time.fixedDeltaTime);
         
         //this boolean is being used to stop the player from being able to throw a psylink at the object if it is moving
         isMoving = Vector3.Distance(transform.position, target) > 0.01f;
